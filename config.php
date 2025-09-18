debaa3c857b57b519e81941cf75075eada47bb8f
Arkoprabho Roy <arkoprabho01@gmail.com>
2025-09-18 13:52:43 +0530

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// config.php - put outside webroot for security and include from get_hospitals.php
-return [
-    'host' => 'localhost',
-    'dbname' => 'hack-2-heal-db',
-    'user' => 'root',
-    'pass' => '',
-    'charset' => 'utf8mb4',
-];
