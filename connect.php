8e529da8bf0271f24724ca5edbef64ab6a6ab699
Arkoprabho Roy <arkoprabho01@gmail.com>
2025-09-18 13:52:56 +0530

    Delete connect.php

--- connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$SERVER_NAME="localhost";
-$USER_NAME="root";
-$PASSWORD="";
-$DATABASE="hack-2-heal-db";
-
-$conn=new PDO("mysql:host=$SERVER_NAME; dbname=$DATABASE",$USER_NAME,$PASSWORD);
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
